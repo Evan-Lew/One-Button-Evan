26e9cf5091e7dae92967bf3fd8aaa3169ad6ccf2
Evan-Lew <evanml.lew@gmail.com>
2022-10-05 21:21:41 -0700

    finished the game restart problem

--- docs/OneButton/main.js
+++ docs/OneButton/main.js
@@ -30,19 +30,15 @@ const G = {
 // Game runtime options
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+    isReplayEnabled: true,
 };
 
 // JSDoc comments for typing
-/**
- * @typedef {{
- * pos: Vector,
- * }} Player
- */
 
-/**
- * @type { Player }
- */
+/** @type {{arrow: Vector}} */
 let player;
+/** @type {Vector[]} */
+let arrows;
 
 /**
  * @typedef {{
@@ -53,56 +49,105 @@ let player;
 /**
  * @type { Yellow_Target [] }
  */
-let yellow_target;
+ let yellow_target;
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * }} Red_Target
+ */
+
+/**
+ * @type { Red_Target [] }
+ */
+ let red_target;
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * }} Blue_Target
+ */
+
+/**
+ * @type { Blue_Target [] }
+ */
+ let blue_target;
+
 
 // The game loop function
 function update() {
     // The init function running at startup
 	if (!ticks) {
-        player = {
-            pos: vec(G.PLAYER_X_POS, G.PLAYER_Y_POS)
-        };   
+        arrows = [vec(G.PLAYER_X_POS, G.PLAYER_Y_POS)];
+        player = { arrow: arrows[0]};    
 
         yellow_target = [];
-
+        const posX = rnd(90, 110);
+        const posY = rnd(20, 30);
         if (yellow_target.length === 0) {
-            const posX = rnd(90, 110);
-            const posY = rnd(20, 30);
-            yellow_target.push({ pos: vec(posX, posY) })
             yellow_target.push({ pos: vec(posX, posY) })
         }
+
+        red_target = [];
+        if (red_target.length === 0) {
+            red_target.push({ pos: vec(posX, posY + 4) })
+            red_target.push({ pos: vec(posX, posY - 4) })
+        }
+
+        // blue_target = [];
+        // if (blue_target.length === 0) {
+        //     blue_target.push({ pos: vec(posX, posY + 4) })
+        //     blue_target.push({ pos: vec(posX, posY - 4) })
+        // }
     }
 
-    yellow_target.forEach((yt) => {
-        color("yellow");
-        box(yt.pos, 2);
-    })
-
-    // Drag the arrow down
-    G.PLAYER_Y_POS += 0.1;
-    player.pos = vec(G.PLAYER_X_POS, G.PLAYER_Y_POS);
-
-    // Move the arrow to the right
-    G.PLAYER_X_POS += 0.5;
-    player.pos = vec(G.PLAYER_X_POS, G.PLAYER_Y_POS);
-
-    // ONE BUTTON MECHANIC
-    if (input.isPressed) {
-        G.PLAYER_Y_POS -= 0.25;
-        player.pos = vec(G.PLAYER_X_POS, G.PLAYER_Y_POS);
+    if (arrows[0].x > 205) {
+        end();
     }
 
-    // Player/Arrow sprite details
-    color ("black");
-    char("a", player.pos);
+    remove(arrows, (a) => {
+        if (input.isPressed) {
+            a.y -= 0.25;
+        }
+        a.x += 0.5;
+        a.y += 0.1;
+        color("black");
+        char("a", a.x, a.y);
+        return a.x > 210;
+    });
 
     remove(yellow_target, (yt) => {
-        const isCollidingwithYTarget = char("a", player.pos).isColliding.rect.yellow;
-        //const isCollidingwithYTarget = box(yt.pos, 2).isColliding.char.a;
+        color("yellow");
+        const isCollidingwithYTarget = box(yt.pos, 2).isColliding.char.a;
         if (isCollidingwithYTarget) {
             color("light_black");
             particle(yt.pos);
+            addScore(10);
+            arrows.pop();
+            arrows.push(vec(G.PLAYER_X_POS, G.PLAYER_Y_POS));
         }
         return (isCollidingwithYTarget);
     })
+
+    remove(red_target, (rt) => {
+        color("red");
+        const isCollidingwithRTarget = box(rt.pos, 3).isColliding.char.a;
+        if (isCollidingwithRTarget) {
+            color("light_black");
+            particle(rt.pos);
+            addScore(5);
+        }
+        return (isCollidingwithRTarget);
+    })
+
+    // remove(blue_target, (bt) => {
+    //     color("blue");
+    //     const isCollidingwithBTarget = box(bt.pos, 2).isColliding.char.a;
+    //     if (isCollidingwithBTarget) {
+    //         color("light_black");
+    //         particle(bt.pos);
+    //         addScore(2);
+    //     }
+    //     return (isCollidingwithBTarget);
+    // })
 }
